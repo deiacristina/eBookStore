8d4a9b82b89a28d1ec80bef95f5e8661e3b6ef06
Andreia Cristina <adeiacristina@icloud.com>
2021-07-23 13:53:10 -0300

    trabalho concluído

---
+++ md.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {});
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
+  });
+
+  var carouselExampleFade = document.querySelector('#carouselExampleFade')
+var carousel = new bootstrap.Carousel(carouselExampleFade)
